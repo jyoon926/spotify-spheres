70fe4b78191beeb808e554fdc6e5010998e655e2
jyoon926 <jacobyoon926@gmail.com>
2024-11-18 13:10:26 -0500

    Update TrackTree

--- src/components/TrackTree.tsx
+++ src/components/TrackTree.tsx
@@ -1,4 +1,3 @@
-import { useEffect, useState } from "react";
 import TrackTreeNode from "./TrackTreeNode";
 import SpotifyWebApi from "spotify-web-api-js";
 import { useTrackTree } from "../utils/TrackTreeContext";
@@ -10,25 +9,6 @@ interface Props {
 
 export default function TrackTree({ spotifyApi }: Props) {
   const { rootNode, initializeTree } = useTrackTree();
-  const [initializing, setInitializing] = useState(false);
-
-  // useEffect(() => {
-  //   const initialize = async () => {
-  //     try {
-  //       spotifyApi.getMyTopTracks({ limit: 1, time_range: "medium_term" }, async function (_, data) {
-  //         initializeTree(data.items[0]);
-  //         setInitializing(false);
-  //       });
-  //     } catch (error) {
-  //       console.error("Error fetching top tracks:", error);
-  //     }
-  //   };
-
-  //   if (!rootNode && !initializing) {
-  //     setInitializing(true);
-  //     initialize();
-  //   }
-  // }, [initializeTree, initializing, rootNode, spotifyApi]);
 
   const handleSelectInitial = (track: SpotifyApi.TrackObjectFull) => {
     initializeTree(track);
