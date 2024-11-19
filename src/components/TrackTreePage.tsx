1e7659f98163bfb5e4ae4b0d4c4d36d61c49c550
jyoon926 <jacobyoon926@gmail.com>
2024-11-19 12:06:49 -0500

    Fix container height

--- src/components/TrackTreePage.tsx
+++ src/components/TrackTreePage.tsx
@@ -12,35 +12,37 @@ interface Props {
 export default function TrackTreePage({ spotifyApi }: Props) {
   return (
     <TrackTreeProvider>
-      <TransformWrapper initialScale={1} limitToBounds={false} minScale={0.25} maxScale={1.5} centerOnInit={true}>
-        {({ resetTransform }) => (
-          <Fragment>
-            <TransformComponent>
-              <div className="w-screen h-screen flex justify-center items-center">
-                <div
-                  className="absolute"
-                  style={{
-                    width: "10000%",
-                    height: "10000%",
-                    backgroundImage: `radial-gradient(circle, rgba(var(--foreground), 0.2) 1px, transparent 1px)`,
-                    backgroundSize: "30px 30px",
-                  }}
-                />
-                <TrackTree spotifyApi={spotifyApi} />
+      <div className="fixed inset-0">
+        <TransformWrapper initialScale={1} limitToBounds={false} minScale={0.25} maxScale={1.5} centerOnInit={true}>
+          {({ resetTransform }) => (
+            <Fragment>
+              <TransformComponent>
+                <div className="flex w-full h-full justify-center items-center">
+                  <div
+                    className="absolute"
+                    style={{
+                      width: "10000%",
+                      height: "10000%",
+                      backgroundImage: `radial-gradient(circle, rgba(var(--foreground), 0.2) 1px, transparent 1px)`,
+                      backgroundSize: "30px 30px",
+                    }}
+                  />
+                  <TrackTree spotifyApi={spotifyApi} />
+                </div>
+              </TransformComponent>
+              <div className="fixed top-2 left-1/2 -translate-x-1/2">
+                <button
+                  onClick={() => resetTransform()}
+                  className="button light sm transition-all duration-200 ease-in-out"
+                >
+                  Reset view
+                </button>
               </div>
-            </TransformComponent>
-            <div className="fixed top-2 left-1/2 -translate-x-1/2">
-              <button
-                onClick={() => resetTransform()}
-                className="button light sm transition-all duration-200 ease-in-out"
-              >
-                Reset view
-              </button>
-            </div>
-          </Fragment>
-        )}
-      </TransformWrapper>
-      <TrackList spotifyApi={spotifyApi} />
+            </Fragment>
+          )}
+        </TransformWrapper>
+        <TrackList spotifyApi={spotifyApi} />
+      </div>
     </TrackTreeProvider>
   );
 }
