06a5bae95846575406f7cff18854bf9c97f00e42
jyoon926 <jacobyoon926@gmail.com>
2024-11-19 14:50:15 -0500

    Test

--- src/components/TrackTreePage.tsx
+++ src/components/TrackTreePage.tsx
@@ -12,7 +12,7 @@ interface Props {
 export default function TrackTreePage({ spotifyApi }: Props) {
   return (
     <TrackTreeProvider>
-      <div className="fixed inset-0">
+      <div className="w-[400px] h-[400px]">
         <TransformWrapper initialScale={10} minScale={5} maxScale={20} centerOnInit={true} wheel={{ step: 2, smoothStep: 0.01 }}>
           {({ centerView }) => (
             <Fragment>
