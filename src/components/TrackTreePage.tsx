1d87b22ae15eb40c52ce9d8526ff1ceaa25fab38
jyoon926 <jacobyoon926@gmail.com>
2024-11-19 11:51:38 -0500

    Use react-zoom-pan-pinch

--- src/components/TrackTreePage.tsx
+++ src/components/TrackTreePage.tsx
@@ -1,8 +1,9 @@
 import SpotifyWebApi from "spotify-web-api-js";
 import TrackTree from "./TrackTree";
-import DraggableContainer from "./DraggableContainer";
 import { TrackTreeProvider } from "../utils/TrackTreeContext";
 import TrackList from "./TrackList";
+import { TransformComponent, TransformWrapper } from "react-zoom-pan-pinch";
+import { Fragment } from "react/jsx-runtime";
 
 interface Props {
   spotifyApi: SpotifyWebApi.SpotifyWebApiJs;
@@ -11,9 +12,34 @@ interface Props {
 export default function TrackTreePage({ spotifyApi }: Props) {
   return (
     <TrackTreeProvider>
-      <DraggableContainer>
-        <TrackTree spotifyApi={spotifyApi} />
-      </DraggableContainer>
+      <TransformWrapper initialScale={1} limitToBounds={false} minScale={0.25} maxScale={1.5} centerOnInit={true}>
+        {({ resetTransform }) => (
+          <Fragment>
+            <TransformComponent>
+              <div className="w-screen h-screen flex justify-center items-center">
+                <div
+                  className="absolute"
+                  style={{
+                    width: "10000%",
+                    height: "10000%",
+                    backgroundImage: `radial-gradient(circle, rgba(var(--foreground), 0.2) 1px, transparent 1px)`,
+                    backgroundSize: "30px 30px",
+                  }}
+                />
+                <TrackTree spotifyApi={spotifyApi} />
+              </div>
+            </TransformComponent>
+            <div className="fixed top-2 left-1/2 -translate-x-1/2">
+              <button
+                onClick={() => resetTransform()}
+                className="button light sm transition-all duration-200 ease-in-out"
+              >
+                Reset view
+              </button>
+            </div>
+          </Fragment>
+        )}
+      </TransformWrapper>
       <TrackList spotifyApi={spotifyApi} />
     </TrackTreeProvider>
   );
